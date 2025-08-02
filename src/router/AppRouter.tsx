bed0f9076bfa3eb3eaa5463af67e75d480dfa6ee
Marlon <marlonmoura3000@gmail.com>
2025-08-02 19:38:55 -0300

    fix

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,22 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ProtectedRoute } from './ProtectedRoute';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
-import FilmesPage from '../pages/FilmesPage';
-import LivrosPage from '../pages/LivrosPage';
 import PlaylistsPage from '../pages/PlaylistsPage';
 import PlaylistDetailPage from '../pages/PlaylistDetailPage';
+import FilmesPage from '../pages/FilmesPage';
+import { ProtectedRoute } from './ProtectedRoute';
+import LivrosPage from '../pages/LivrosPage';
 
 export const AppRouter = () => {
-
-  const repoName = '/Catalogo';
-
   return (
-    <BrowserRouter basename={repoName}>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        
+
         <Route element={<ProtectedRoute />}>
           <Route path="/playlists" element={<PlaylistsPage />} />
           <Route path="/playlists/:id" element={<PlaylistDetailPage />} />
@@ -25,6 +22,6 @@ export const AppRouter = () => {
           <Route path="/" element={<PlaylistsPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
